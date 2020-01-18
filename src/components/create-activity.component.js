d345b9c99320b2e92dfb4deb908cbf15e6bb21cb
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2020-01-19 01:05:10 +0300

    forms added

--- src/components/create-activity.component.js
+++ src/components/create-activity.component.js
@@ -1,62 +1,132 @@
 import React, { Component } from "react";
+import DatePicker from 'react-datepicker';
+
 
 export default class CreateActivity extends Component {
-  constructor(props){
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.state = {
-          username : '',
-          desc:'',
-          price:0,
-          date: new Date(),
-          users:[]
-      }
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDesc = this.onChangeDesc.bind(this);
+    this.onChangePrice = this.onChangePrice.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+      desc: "",
+      price: 0,
+      date: new Date(),
+      users: []
+    };
   }
 
-  onChangeUsername(e){
-      this.setState({
-          username:e.target.value
-      });
+  componentDidMount() {
+    this.setState({
+      users: ["test user"],
+      username: "test user"
+    });
   }
 
-  onChangeDesc(e){
-      this.setState({
-          desc:e.target.value
-      });
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
   }
 
-  onChangePrice(e){
-      this.setState({
-          price:e.target.value
-      });
+  onChangeDesc(e) {
+    this.setState({
+      desc: e.target.value
+    });
   }
 
-  onChangeDate(date){
-      this.setState({
-          date:date
-      });
+  onChangePrice(e) {
+    this.setState({
+      price: e.target.value
+    });
   }
 
-  onSubmit(e){
-      e.preventDefault();
-      
-      const activity = {
-          username: this.state.username,
-          desc:this.state.desc,
-          price:this.state.price,
-          date:this.state.date
-      }
-      console.log(activity);
-      
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    });
   }
-   
-    render() {
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const activity = {
+      username: this.state.username,
+      desc: this.state.desc,
+      price: this.state.price,
+      date: this.state.date
+    };
+    console.log(activity);
+
+    window.location = "/";
+  }
+
+  render() {
     return (
-    
-        <div>
-            <p>You are on Create Activity Component</p>
-        </div>
-         
+      <div>
+        <h3>Create New Payment Log</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <select
+              ref="userInput"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            >
+              {this.state.users.map(function(user) {
+                return (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Description: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+            />
+          </div>
+          <div className="form-group">
+            <label>Duration (in minutes): </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.duration}
+              onChange={this.onChangeDuration}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.onChangeDate}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create Payment Log"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
     );
   }
 }
