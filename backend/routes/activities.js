16acf1063120f83c907875ddcebbefa2f7400d05
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2020-01-18 19:47:40 +0300

    update/delete added to acttivities

--- backend/routes/activities.js
+++ backend/routes/activities.js
@@ -14,12 +14,40 @@ router.route('/add').post((req, res) => {
     const date = Date.parse(req.body.date);
 
     const newActivity = new Activity({
-        username,desc,price,date
+        username, desc, price, date
     });
 
     newActivity.save()
-    .then(()=> res.json('Activity added'))
-    .catch(err=>res.status(400).json('Error: '+ err));
+        .then(() => res.json('Activity added'))
+        .catch(err => res.status(400).json('Error: ' + err));
+
+    router.route('/:id').get((req, res) => {
+        Activity.findById(req.params.id)
+            .then(activity => res.json(activity))
+            .catch(err => res.status(400).json('Error: ' + err));
+    });
+
+    router.route('/:id').delete((req, res) => {
+        Activity.findByIdAndDelete(req.params.id)
+            .then(() => res.json('Activity deleted'))
+            .catch(err => res.status(400).json('Error: ' + err));
+    });
+
+    router.route('/update/:id').post((req, rest) => {
+        Activity.findById(req.params.id)
+            .then(activity => {
+                activity.username = req.body.username;
+                activity.desc = req.body.desc;
+                activity.price = Number(req.body.price);
+                activity.date = Date.parse(req.body.date);
+
+                activity.save()
+                    .then(() => res.json('Activity updated!'))
+                    .catch(err => res.status(400).json('Error: ' + err));
+            })
+
+    })
+        .catch(err => res.status(400).json('Error' + err));
 });
 
 module.exports = router;
