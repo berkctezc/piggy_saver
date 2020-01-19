a154d4664006bbfb2b1390db14a00836605328d7
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2020-01-19 03:35:07 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 import Navbar from "./components/navbar.component";
 import ActivitiesList from "./components/activities-list.component";
 import EditActivity from "./components/edit-activity.component";
@@ -15,9 +14,9 @@ import "./App.css";
 function App() {
   return (
     <Router>
+      <Navbar />
+      <br />
       <div className="container">
-        <Navbar />
-        <br />
         <Route path="/" exact component={ActivitiesList} />
         <Route path="/edit/:id" exact component={EditActivity} />
         <Route path="/create" exact component={CreateActivitiy} />
--- src/components/edit-activity.component.js
+++ src/components/edit-activity.component.js
@@ -1,13 +1,156 @@
 import React, { Component } from "react";
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
-export default class EditActivities extends Component {
+
+export default class EditActivity extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDesc = this.onChangeDesc.bind(this);
+    this.onChangePrice = this.onChangePrice.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+      desc: "",
+      price: 0,
+      date: new Date(),
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/activities/'+this.props.match.params.id)
+    .then(response => {
+      this.setState({
+        username: response.data.username,
+        desc: response.data.desc,
+        duration: response.data.price,
+        date: new Date(response.data.date)
+      })   
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+
+
+    axios.get('http://localhost:5000/users/')
+    .then(response=>{
+        if(response.data.length>0){
+            this.setState({
+                users:response.data.map(user=>user.username),
+            })
+        }
+    })
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onChangeDesc(e) {
+    this.setState({
+      desc: e.target.value
+    });
+  }
+
+  onChangePrice(e) {
+    this.setState({
+      price: e.target.value
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const activity = {
+      username: this.state.username,
+      desc: this.state.desc,
+      price: this.state.price,
+      date: this.state.date
+    };
+
+    console.log(activity);
+
+    axios.post('http://localhost:5000/activities/update/'+this.props.match.params.id, activity)
+            .then(res => console.log(res.data));
+    
+    //window.location = "/";
+  }
+
   render() {
     return (
-    
-        <div>
-            <p>You are on Edit Activities Component</p>
-        </div>
-         
+      <div>
+        <h3>Edit Payment Log</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <select
+              ref="userInput"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            >
+              {this.state.users.map(function(user) {
+                return (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Description: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.desc}
+              onChange={this.onChangeDesc}
+            />
+          </div>
+          <div className="form-group">
+            <label>Price: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.price}
+              onChange={this.onChangePrice}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.onChangeDate}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Edit Payment Log"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
     );
   }
 }
